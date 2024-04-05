793ac2b07455ee6c39569f33af313399c222c9a6
mgavin <matthewgavin489@gmail.com>
2024-04-05 00:50:16 -0500

    Needs commentary.

--- LogGoalReplayTime/LogGoalReplayTime.cpp
+++ LogGoalReplayTime/LogGoalReplayTime.cpp
@@ -13,51 +13,60 @@ void LogGoalReplayTime::onLoad() {
         // initialize things
         _globalCvarManager        = cvarManager;
         HookedEvents::gameWrapper = gameWrapper;
-        logbook.open(gameWrapper->GetDataFolder().append("GoalReplayTime.csv"));
-        if (!logbook) {
-                LOG("UNABLE TO OPEN LOGBOOK TO LOG GOAL REPLAY TIMES! CANNOT MOVE FORWARD!");
-                LOG("TRY RELOADING THE PLUGIN: plugin reload/load LogGoalReplayTime");
 
-                return;
-        }
+        init_logfile();
 
         HookedEvents::AddHookedEvent(
                 "Function GameEvent_Soccar_TA.PostGoalScored.BeginState",
-                [](std::string eventName) {
-                        // AFTER GOAL IS SCORED !
-                },
+                // AFTER GOAL IS SCORED ! and it shows "XYZ SCORED!"
+                std::bind(&LogGoalReplayTime::HandlePostGoalScoredBegin, this),
                 true);
         HookedEvents::AddHookedEvent(
                 "Function GameEvent_Soccar_TA.PostGoalScored.EndState",
-                [](std::string eventName) {
-                        // idk yet
-                        // WHEN THE "XYZ" SCORED" TEXT IS OVER!!!
-                },
+                // WHEN THE "XYZ" SCORED" TEXT IS OVER!!!
+                std::bind(&LogGoalReplayTime::HandlePostGoalScoredEnd, this),
                 true);
         HookedEvents::AddHookedEvent(
-                "Function GameEvred.BeginState",
-                [](std::string eventName) {
-                        // AFTER GOAL IS SCORED !
-                },
+                "Function GameEvent_Soccar_TA.ReplayPlayback.BeginState",
+                // WHEN THE REPLAY BEGINS
+                std::bind(&LogGoalReplayTime::HandleGoalReplayPlaybackBegin, this),
                 true);
         HookedEvents::AddHookedEvent(
-                "Function GameEvent_Soccar_TA.PostGoalScored.BeginState",
-                [](std::string eventName) {
-                        // AFTER GOAL IS SCORED !
-                },
+                "Function TAGame.PlayerController_TA.ReadyUp",
+                // IF THE PLAYER HITS "SKIP REPLAY"
+                std::bind(&LogGoalReplayTime::HandlePlayerReadyUp, this),
                 true);
         HookedEvents::AddHookedEvent(
-                "Function GameEvent_Soccar_TA.PostGoalScored.BeginState",
-                [](std::string eventName) {
-                        // AFTER GOAL IS SCORED !
-                },
+                "Function TAGame.PRI_TA.ServerReadyUp",
+                // THE TEAM SKIPPED THE REPLAY, AKA THEY'RE "READY"!
+                std::bind(&LogGoalReplayTime::HandleServerReadyUp, this),
                 true);
         HookedEvents::AddHookedEvent(
-                "Function GameEvent_Soccar_TA.PostGoalScored.BeginState",
-                [](std::string eventName) {
-                        // AFTER GOAL IS SCORED !
-                },
+                "Function GameEvent_Soccar_TA.ReplayPlayback.EndState",
+                // REPLAY IS OVER!
+                std::bind(&LogGoalReplayTime::HandleGoalReplayPlaybackEnd, this),
                 true);
+        HookedEvents::AddHookedEvent(
+                "Function GameEvent_Soccar_TA.Countdown.BeginState",
+                // COUNTDOWN BEGINS!
+                std::bind(&LogGoalReplayTime::HandleCountdownBegin, this),
+                true);
+        HookedEvents::AddHookedEvent(
+                "Function GameEvent_Soccar_TA.Countdown.EndState",
+                // COWNTDOWN ENDS! 3-2-1 GO!
+                std::bind(&LogGoalReplayTime::HandleCountdownEnd, this),
+                true);
+        HookedEvents::AddHookedEvent(
+                "Function TAGame.LoadingScreen_TA.HandlePreLoadMap",
+                // went back to main menu
+                std::bind(&LogGoalReplayTime::HandleBackToMainMenu, this),
+                true);
+
+        memset(&current_data, 0, sizeof(data));
+        current_data.game_guid = "";
+        memset(&stats_data, 0, sizeof(data));
+        stats_data.game_guid = "";
+        generate_stats();
 }
 
 /// <summary>
@@ -76,6 +85,37 @@ void LogGoalReplayTime::onUnload() {
 /// </summary>
 void LogGoalReplayTime::RenderSettings() {
         // for imgui plugin window
+
+        // grab stats once, somehow
+
+        // output stats
+        ImGui::TextUnformatted(
+                std::vformat("Last Game Number: {}", std::make_format_args(stats_data.game_num)).c_str());
+        ImGui::TextUnformatted(std::vformat("Last Game GUID: {}", std::make_format_args(stats_data.game_guid)).c_str());
+        ImGui::TextUnformatted(
+                std::vformat(
+                        "Time (in milliseconds) spent after goal scored: Min: {:6d} | Average: {:6d} | Max: {:6d}",
+                        std::make_format_args(
+                                stats_data.min_post_goal,
+                                stats_data.milliseconds_spent_post_goal,
+                                stats_data.max_post_goal))
+                        .c_str());
+        ImGui::TextUnformatted(
+                std::vformat(
+                        "Time (in milliseconds) spent after goal replay: Min: {:6d} | Average: {:6d} | Max: {:6d}",
+                        std::make_format_args(
+                                stats_data.min_goal_replay,
+                                stats_data.milliseconds_spent_goal_replay,
+                                stats_data.max_goal_replay))
+                        .c_str());
+        ImGui::TextUnformatted(
+                std::vformat(
+                        "Time (in milliseconds) spent after goal countdown: Min: {:6d} | Average: {:6d} | Max: {:6d}",
+                        std::make_format_args(
+                                stats_data.min_countdown,
+                                stats_data.milliseconds_spent_countdown,
+                                stats_data.max_countdown))
+                        .c_str());
 }
 
 /// <summary>
@@ -96,6 +136,201 @@ void LogGoalReplayTime::SetImGuiContext(uintptr_t ctx) {
 
 std::string LogGoalReplayTime::GetPluginName() {
         return "LogGoalReplayTime";
+}
+
+void LogGoalReplayTime::init_logfile() {
+        if (!std::filesystem::exists(LOGBOOK_FILE_PATH)) {
+                // file aint written to yet?
+                std::ofstream  logbook_file {LOGBOOK_FILE_PATH, std::ios::app};
+                csv::CSVWriter logbook_writer {logbook_file};
+                logbook_writer << std::vector {
+                        "LoggedGame#",
+                        "LoggedGameGUID",
+                        "MillisecondsSpentPostGoal",
+                        "MillisecondsSpentGoalReplay",
+                        "DidPlayerSkip",
+                        "DidTeamSkip",
+                        "MillisecondsSpentCountdown"};
+        } else {
+                csv::CSVFileInfo fi = csv::get_file_info(LOGBOOK_FILE_PATH.string());
+                LOG("fi info: ncols {}, nrows {}", fi.n_cols, fi.n_rows);
+                if (fi.n_rows > 1) {
+                        csv::CSVReader logbook_reader {LOGBOOK_FILE_PATH.string()};
+                        csv::CSVRow    row;
+                        while (logbook_reader.read_row(row))
+                                ;
+                        current_data.game_num  = row["LoggedGame#"].get<int>();
+                        current_data.game_guid = row["LoggedGameGUID"].get<std::string>();
+                }
+        }
+}
+
+void LogGoalReplayTime::HandlePostGoalScoredBegin() {
+        in_post_goal_scored = true;
+
+        if (gameWrapper->IsInOnlineGame()) {
+                // if you're in an online game
+                // why not freeplay? idk...
+                csv::CSVFileInfo fi = csv::get_file_info(LOGBOOK_FILE_PATH.string());
+                if (fi.n_rows > 1) {  // COUNTS THE HEADER ROW
+                        csv::CSVReader logbook_reader {LOGBOOK_FILE_PATH.string()};
+                        /* skip to end :rolling_eyes: */
+                        csv::CSVRow    row;
+                        while (logbook_reader.read_row(row))
+                                ;
+                        current_data.game_num  = row["LoggedGame#"].get<int>();
+                        current_data.game_guid = row["LoggedGameGUID"].get<std::string>();
+                }
+                ServerWrapper sw = gameWrapper->GetCurrentGameState();
+                if (sw) {
+                        std::string game_guid = sw.GetMatchGUID();
+                        if (game_guid != current_data.game_guid) {
+                                // if you're in a new match
+                                current_data.game_guid  = game_guid;
+                                current_data.game_num  += 1;
+                        }
+                }
+
+                t = std::chrono::steady_clock::now();
+        }
+}
+void LogGoalReplayTime::HandlePostGoalScoredEnd() {
+        if (in_post_goal_scored) {
+                did_post_goal                            = true;
+                in_post_goal_scored                      = false;
+                const std::chrono::duration<double> diff = std::chrono::steady_clock::now() - t;
+                current_data.milliseconds_spent_post_goal =
+                        static_cast<int>(std::chrono::duration_cast<std::chrono::milliseconds>(diff).count());
+        }
+}
+
+void LogGoalReplayTime::HandleGoalReplayPlaybackBegin() {
+        in_goal_replay = true;
+        t              = std::chrono::steady_clock::now();
+}
+
+void LogGoalReplayTime::HandlePlayerReadyUp() {
+        current_data.did_player_skip = true;
+}
+
+void LogGoalReplayTime::HandleServerReadyUp() {
+        current_data.did_team_skip = true;
+}
+
+void LogGoalReplayTime::HandleGoalReplayPlaybackEnd() {
+        if (in_goal_replay) {
+                in_goal_replay                           = false;
+                did_goal_replay                          = true;
+                const std::chrono::duration<double> diff = std::chrono::steady_clock::now() - t;
+                current_data.milliseconds_spent_goal_replay =
+                        static_cast<int>(std::chrono::duration_cast<std::chrono::milliseconds>(diff).count());
+        }
+}
+
+void LogGoalReplayTime::HandleCountdownBegin() {
+        in_countdown = true;
+        t            = std::chrono::steady_clock::now();
+}
+
+void LogGoalReplayTime::HandleCountdownEnd() {
+        if (in_countdown) {
+                in_countdown                             = false;
+                did_countdown                            = true;
+                const std::chrono::duration<double> diff = std::chrono::steady_clock::now() - t;
+                current_data.milliseconds_spent_countdown =
+                        static_cast<int>(std::chrono::duration_cast<std::chrono::milliseconds>(diff).count());
+
+                if (did_post_goal && did_goal_replay && did_countdown) {
+                        write_and_flush();
+                }
+        }
+}
+
+void LogGoalReplayTime::HandleBackToMainMenu() {
+        if (in_post_goal_scored || in_goal_replay || in_countdown) {
+                // somehow got kicked back to the main manu without going through a countdown
+                // maybe afk?, or the goal and replay was the last one of the game
+                write_and_flush();
+                in_post_goal_scored = in_goal_replay = in_countdown = false;
+        }
+}
+
+void LogGoalReplayTime::write_and_flush() {
+        // write everything out
+        std::ofstream logbook_file {LOGBOOK_FILE_PATH, std::ios::app};  // fuck. this should be checked.
+        if (!logbook_file) {
+                LOG("CANNOT OPEN LOGBOOK FILE! THROWING AWAY DATA! CURRENT DATA:");
+                LOG("Game Number: {}\nGame GUID: {}\nMilliseconds Spent Post Goal: {}\nMilliseconds Spent Goal Replay: {}\nMilliseconds Spent In Countdown: {}\n Player Skipped: {}\n Team Skipped: {}\n",
+                    current_data.game_num,
+                    current_data.game_guid,
+                    current_data.milliseconds_spent_post_goal,
+                    current_data.milliseconds_spent_goal_replay,
+                    current_data.milliseconds_spent_countdown,
+                    current_data.did_player_skip,
+                    current_data.did_team_skip);
+        } else {
+                csv::CSVWriter logbook_writer {logbook_file};
+                logbook_writer << std::vector<std::string> {
+                        std::to_string(current_data.game_num),
+                        current_data.game_guid,
+                        std::to_string(current_data.milliseconds_spent_post_goal),
+                        std::to_string(current_data.milliseconds_spent_goal_replay),
+                        std::to_string(current_data.did_player_skip),
+                        std::to_string(current_data.did_team_skip),
+                        std::to_string(current_data.milliseconds_spent_countdown)};
+        }
+        // clear everything out.
+        memset(&current_data, 0, sizeof(data));
+        current_data.game_guid = "";
+        did_post_goal = did_goal_replay = did_countdown = false;
+        generate_stats();
+}
+
+void LogGoalReplayTime::generate_stats() {
+        csv::CSVFileInfo fi = csv::get_file_info(LOGBOOK_FILE_PATH.string());
+        if (fi.n_rows < 2) {
+                return;
+        }
+        csv::CSVReader logbook_reader(LOGBOOK_FILE_PATH.string());
+
+        using ull           = unsigned long long;
+        ull         tmppost = 0, tmpgoal = 0, tmpcount = 0;
+        int         tmppostc = 0, tmpgoalc = 0, tmpcountc = 0;
+        csv::CSVRow row;
+        while (logbook_reader.read_row(row)) {
+                if (!row["DidTeamSkip"].get<bool>()) {
+                        if (auto n = row["MillisecondsSpentPostGoal"].get<int>(); n != 0) {
+                                tmppost  += n;
+                                tmppostc += 1;
+                                stats_data.min_post_goal =
+                                        (stats_data.min_post_goal == 0) ? n : std::min(stats_data.min_post_goal, n);
+                                stats_data.max_post_goal =
+                                        (stats_data.max_post_goal == 0) ? n : std::max(stats_data.max_post_goal, n);
+                        }
+                        if (auto n = row["MillisecondsSpentGoalReplay"].get<int>(); n != 0) {
+                                tmpgoal  += n;
+                                tmpgoalc += 1;
+                                stats_data.min_goal_replay =
+                                        (stats_data.min_goal_replay == 0) ? n : std::min(stats_data.min_goal_replay, n);
+                                stats_data.max_goal_replay =
+                                        (stats_data.max_goal_replay == 0) ? n : std::max(stats_data.max_goal_replay, n);
+                        }
+                        if (auto n = row["MillisecondsSpentCountdown"].get<int>(); n != 0) {
+                                tmpcount  += n;
+                                tmpcountc += 1;
+                                stats_data.min_countdown =
+                                        (stats_data.min_countdown == 0) ? n : std::min(stats_data.min_countdown, n);
+                                stats_data.max_countdown =
+                                        (stats_data.max_countdown == 0) ? n : std::max(stats_data.max_countdown, n);
+                        }
+                }
+        }
+
+        stats_data.milliseconds_spent_post_goal   = static_cast<int>(tmppost / static_cast<ull>(tmppostc));
+        stats_data.milliseconds_spent_goal_replay = static_cast<int>(tmpgoal / static_cast<ull>(tmpgoalc));
+        stats_data.milliseconds_spent_countdown   = static_cast<int>(tmpcount / static_cast<ull>(tmpcountc));
+        stats_data.game_num                       = row["LoggedGame#"].get<int>();
+        stats_data.game_guid                      = row["LoggedGameGUID"].get<std::string>();
 }
 
 /*
--- LogGoalReplayTime/LogGoalReplayTime.h
+++ LogGoalReplayTime/LogGoalReplayTime.h
@@ -1,14 +1,61 @@
 #pragma once
 
+#include <chrono>
 #include <fstream>
 #include "bakkesmod/imgui/imgui.h"
 #include "bakkesmod/plugin/bakkesmodplugin.h"
 #include "bakkesmod/plugin/PluginSettingsWindow.h"
+#include "csv.hpp"
 #include "HookedEvents.h"
 
 class LogGoalReplayTime : public BakkesMod::Plugin::BakkesModPlugin, public BakkesMod::Plugin::PluginSettingsWindow {
 private:
-        std::ofstream logbook;
+        struct data {
+                int         game_num;
+                std::string game_guid;
+                int         milliseconds_spent_post_goal;
+                int         milliseconds_spent_goal_replay;
+                bool        did_player_skip;
+                bool        did_team_skip;
+                int         milliseconds_spent_countdown;
+        };
+
+        struct stats : public data {
+                int max_post_goal;
+                int min_post_goal;
+                int max_goal_replay;
+                int min_goal_replay;
+                int max_countdown;
+                int min_countdown;
+        };
+
+        const std::filesystem::path LOGBOOK_FILE_PATH = gameWrapper->GetDataFolder().append("LogGoalReplayTime.csv");
+
+        bool in_post_goal_scored = false;
+        bool in_goal_replay      = false;
+        bool in_countdown        = false;
+        bool did_post_goal       = false;
+        bool did_goal_replay     = false;
+        bool did_countdown       = false;
+
+        std::chrono::time_point<std::chrono::steady_clock> t;
+
+        data  current_data;
+        stats stats_data;
+
+        void init_logfile();
+        void write_and_flush();
+        void generate_stats();
+
+        void HandlePostGoalScoredBegin();
+        void HandlePostGoalScoredEnd();
+        void HandleGoalReplayPlaybackBegin();
+        void HandlePlayerReadyUp();
+        void HandleServerReadyUp();
+        void HandleGoalReplayPlaybackEnd();
+        void HandleCountdownBegin();
+        void HandleCountdownEnd();
+        void HandleBackToMainMenu();
 
 public:
         void onLoad() override;
@@ -17,7 +64,6 @@ public:
         void        RenderSettings() override;
         std::string GetPluginName() override;
         void        SetImGuiContext(uintptr_t ctx) override;
-
         //
         // inherit from
         //				public BakkesMod::Plugin::PluginWindow
